cad5656db9b1825b60e1243ce39e0575d45e42a1
banjun <banjun@gmail.com>
2023-10-19 21:33:25 +0900

    Widget template created on Xcode 15.1b

---
+++ WidgetA/WidgetABundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetABundle.swift
+//  WidgetA
+//
+//  Created by BAN Jun on R 5/10/19.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetABundle: WidgetBundle {
+    var body: some Widget {
+        WidgetA()
+        WidgetALiveActivity()
+    }
+}

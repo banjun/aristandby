7753cb705dcda2de112be69febebcb3660632b9c
banjun <banjun@gmail.com>
2023-10-19 20:25:11 +0900

    Initial Commit

---
+++ Widget17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Widget17
+//
+//  Created by BAN Jun on R 5/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Widget17/Widget17App.swift
@@ -0,0 +1,17 @@
+//
+//  Widget17App.swift
+//  Widget17
+//
+//  Created by BAN Jun on R 5/10/19.
+//
+
+import SwiftUI
+
+@main
+struct Widget17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
